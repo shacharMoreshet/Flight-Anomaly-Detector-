59339a208c30e24e5c0a0f2d617f922d3eb7ba3b
ShacharMoreshet <shachar.moreshet@live.biu.ac.il>
2021-10-12 13:59:53 +0300

    h

---
+++ ass1/cmake-build-debug/CMakeFiles/ass1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass1.dir/main.cpp.obj"
+  "ass1.exe"
+  "ass1.exe.manifest"
+  "ass1.lib"
+  "ass1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
